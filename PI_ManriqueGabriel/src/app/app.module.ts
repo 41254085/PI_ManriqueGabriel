743d0094832b2583fccae5197d0c76e7b1dcec33
41254085 <gabimanrique13@gmail.com>
2023-05-15 18:37:26 -0300

    Componentes

--- PI_ManriqueGabriel/src/app/app.module.ts
+++ PI_ManriqueGabriel/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentesComponent } from './componentes/componentes.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoApComponent } from './componente/logo-ap/logo-ap.component';
+import { SocialComponent } from './componentes/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentesComponent,
+    HeaderComponent,
+    LogoApComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
